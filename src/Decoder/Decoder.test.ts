9ebad23dbf8686c256b633d6b5c0034baa3d6f68
bahamut657 <jacopo.vichi@gmail.com>
2023-07-14 01:00:49 +0200

    Decoder adapted for singleton and reusability

--- src/Decoder/Decoder.test.ts
+++ src/Decoder/Decoder.test.ts
@@ -1,4 +1,4 @@
-import MadMessageDecoder, { DECODER_ERRORS } from "."
+import MadMessageDecoder, { DECODER_ERRORS, DecodedMessage } from "."
 
 const HELLO_WORLD = 'ZjcwZWVjYTUtZjVhZS00ZDllLWFmNjktYWExNmYyNWY4NjRjICAgICAgICAgMDFqICAgICAgICAgICAgICAgICAgMTcweyJoZWxsbyI6IndvcmxkIn0=';
 
@@ -18,89 +18,115 @@ const sleep = (ms: number) => {
 
 describe("Decoder lifecycle", () => {
     it("spawns a decoder instance", () => {
-        const spy = jest.fn()
-        const decoder = new MadMessageDecoder(Buffer.from("hello world"), spy, false);
+        const spy = jest.fn((e: Error | null, decodedMessage?: DecodedMessage) => {
+            expect(e instanceof Error).toBeTruthy();
+            if (e) {
+                expect(e.message).toEqual(DECODER_ERRORS.WRONG_MESSAGE_FORMAT)
+            }
+            expect(decodedMessage).toBeUndefined();
+        })
+        const decoder = new MadMessageDecoder(false);
+        decoder.read(Buffer.from("hello world"), spy)
         expect(spy).toHaveBeenCalledTimes(1);
-        expect(spy).toHaveBeenCalledWith(new Error(DECODER_ERRORS.WRONG_MESSAGE_FORMAT), decoder)
-        expect(decoder.get_attr('id')).toBeUndefined();
-        expect(decoder.get_attr('type')).toBeUndefined();
-        expect(decoder.get_attr('content')).toBeUndefined();
-        expect(decoder.get_attr('finalize')).toBeFalsy();
-        expect(decoder.get_attr('isResponse')).toBeTruthy();
-        expect(decoder.get_attr('num')).toEqual(0);
-        expect(decoder.get_attr('length')).toEqual(0);
-        expect(decoder.get_attr('transferredLength')).toEqual(0);
-        expect(decoder.get_attr('zip')).toEqual("0");
+
     })
 
     it("decodes hello world json message", () => {
-        const spy = jest.fn()
-        const decoder = new MadMessageDecoder(Buffer.from(HELLO_WORLD, 'base64'), spy, false);
+        const spy = jest.fn((e: Error | null, decodedMessage?: DecodedMessage) => {
+            expect(e === null).toBeTruthy();
+            expect(decodedMessage).toBeDefined();
+            if (decodedMessage) {
+                expect(decodedMessage.content).toBeDefined();
+                expect(typeof decodedMessage.content).toEqual("object")
+                expect(decodedMessage.content.hello).toEqual("world")
+            }
+        })
+        const decoder = new MadMessageDecoder(false);
+        decoder.read(Buffer.from(HELLO_WORLD, 'base64'), spy)
         expect(spy).toHaveBeenCalledTimes(1);
-        expect(spy).toHaveBeenCalledWith(null, decoder);
-        expect(decoder.content).toBeDefined();
-        expect(typeof decoder.content).toEqual("object")
-        expect(decoder.content.hello).toEqual("world")
 
     })
+
 
     it("decodes hello world json gzipped message", async () => {
-        const spy = jest.fn()
-        const decoder = new MadMessageDecoder(Buffer.from(GZIP_HELLO_WORLD, 'base64'), spy, false);
+        const spy = jest.fn((e: Error | null, decodedMessage?: DecodedMessage) => {
+            expect(e === null).toBeTruthy();
+            expect(decodedMessage).toBeDefined();
+            if (decodedMessage) {
+                expect(decodedMessage.content).toBeDefined();
+                expect(typeof decodedMessage.content).toEqual("object")
+                expect(decodedMessage.content.hello).toEqual("world")
+            }
+        })
+        const decoder = new MadMessageDecoder(false);
+        decoder.read(Buffer.from(GZIP_HELLO_WORLD, 'base64'), spy)
         await sleep(1000)
         expect(spy).toHaveBeenCalledTimes(1);
-        expect(spy).toHaveBeenCalledWith(null, decoder);
-        expect(decoder.content).toBeDefined();
-        expect(typeof decoder.content).toEqual("object")
-        expect(decoder.content.hello).toEqual("world")
     })
 
+
     it("decodes hello world json deflated message", async () => {
-        const spy = jest.fn()
-        const decoder = new MadMessageDecoder(Buffer.from(DEFLATE_HELLO_WORLD, 'base64'), spy, false);
+        const spy = jest.fn((e: Error | null, decodedMessage?: DecodedMessage) => {
+            expect(e === null).toBeTruthy();
+            expect(decodedMessage).toBeDefined();
+            if (decodedMessage) {
+                expect(decodedMessage.content).toBeDefined();
+                expect(typeof decodedMessage.content).toEqual("object")
+                expect(decodedMessage.content.hello).toEqual("world")
+            }
+        })
+        const decoder = new MadMessageDecoder(false);
+        decoder.read(Buffer.from(DEFLATE_HELLO_WORLD, 'base64'), spy)
         await sleep(1000);
         expect(spy).toHaveBeenCalledTimes(1);
-        expect(spy).toHaveBeenCalledWith(null, decoder);
-        expect(decoder.content).toBeDefined();
-        expect(typeof decoder.content).toEqual("object")
-        expect(decoder.content.hello).toEqual("world")
     })
 
     it("decodes binary deflated message", async () => {
-        const spy = jest.fn()
-        const decoder = new MadMessageDecoder(Buffer.from(DEFLATE_BINARY_HELLO_WORLD, 'base64'), spy, false);
+        const spy = jest.fn((e: Error | null, decodedMessage?: DecodedMessage) => {
+            expect(e === null).toBeTruthy();
+            expect(decodedMessage).toBeDefined();
+            if (decodedMessage) {
+                expect(decodedMessage.content).toBeDefined();
+                expect(decodedMessage.content instanceof Buffer).toBeTruthy()
+                expect(decodedMessage.content.toString("utf-8")).toEqual("HelloWorld!")
+            }
+        })
+        const decoder = new MadMessageDecoder(false);
+        decoder.read(Buffer.from(DEFLATE_BINARY_HELLO_WORLD, 'base64'), spy)
         await sleep(1000);
         expect(spy).toHaveBeenCalledTimes(1);
-        expect(spy).toHaveBeenCalledWith(null, decoder);
-        expect(decoder.content).toBeDefined();
-        expect(decoder.content instanceof Buffer).toBeTruthy()
-        expect(decoder.content.toString("utf-8")).toEqual("HelloWorld!")
     })
 
     it("decodes hello world json brotli message", async () => {
-        const spy = jest.fn()
-        const decoder = new MadMessageDecoder(Buffer.from(BROTLI_HELLO_WORLD, 'base64'), spy, true);
+        const spy = jest.fn((e: Error | null, decodedMessage?: DecodedMessage) => {
+            expect(e === null).toBeTruthy();
+            expect(decodedMessage).toBeDefined();
+            if (decodedMessage) {
+                expect(decodedMessage.content).toBeDefined();
+                expect(typeof decodedMessage.content).toEqual("object")
+                expect(decodedMessage.content.hello).toEqual("world")
+            }
+        })
+        const decoder = new MadMessageDecoder(true);
+        decoder.read(Buffer.from(BROTLI_HELLO_WORLD, 'base64'), spy)
         await sleep(1000);
         expect(spy).toHaveBeenCalledTimes(1);
-        expect(spy).toHaveBeenCalledWith(null, decoder);
-        expect(decoder.content).toBeDefined();
-        expect(typeof decoder.content).toEqual("object")
-        expect(decoder.content.hello).toEqual("world")
     })
 
     it("fails due to decompression error", async () => {
-        const spy = jest.fn()
-        const decoder = new MadMessageDecoder(Buffer.from(BROTLI_HELLO_WORLD, 'base64'), spy, true);
-        await sleep(1000);
-        expect(spy).toHaveBeenCalledTimes(1);
-        expect(spy).toHaveBeenCalledWith(null, decoder);
-        spy.mockClear();
+        const spy = jest.fn((e: Error | null, decodedMessage?: DecodedMessage) => {
+            expect(e instanceof Error).toBeTruthy();
+            if (e) {
+                expect(e.message).toEqual(DECODER_ERRORS.ZIP_DECOMPRESSION_ERROR)
+            }
+        })
+        const decoder = new MadMessageDecoder(true);
         decoder.unzip_cmd = jest.fn(() => (content, cb) => {
             cb(new Error("Emulated zip error"), Buffer.from(""));
         });
+
         decoder.read(Buffer.from(BROTLI_HELLO_WORLD, 'base64'), spy)
         expect(spy).toHaveBeenCalledTimes(1);
-        expect(spy).toHaveBeenCalledWith(new Error(DECODER_ERRORS.ZIP_DECOMPRESSION_ERROR), decoder);
     })
 
 })
--- src/Decoder/Decoder.ts
+++ src/Decoder/Decoder.ts
@@ -1,7 +1,20 @@
 import zlib from "node:zlib"
 import { ZlibMethod, type MadMessageCompression, type MadMessageType } from '../types'
+import { decode } from "node:punycode";
 
-type DecoderCallback = (error: Error | null, message: MadMessageDecoder) => void
+export type DecodedMessage = {
+  id: string;
+  type: MadMessageType;
+  content: any;
+  finalize: boolean;
+  isResponse?: boolean;
+  num: number;
+  length: number;
+  transferredLength: number;
+  zip: MadMessageCompression;
+}
+
+type DecoderCallback = (error: Error | null, message?: DecodedMessage) => void
 
 export const DECODER_ERRORS = {
   WRONG_MESSAGE_FORMAT: "wrong message format",
@@ -10,40 +23,11 @@ export const DECODER_ERRORS = {
 }
 
 class MadMessageDecoder {
-  /** message id */
-  id?: string
-  /** message type */
-  type?: MadMessageType
-  /** message content (buffer or json) */
-  content: any
-  /** if true, the message is finished */
-  finalize: boolean
-  /** if true, is a response message */
-  isResponse: boolean
-  /** message packet number */
-  num: number
-  /** message length */
-  length: number
-  /** message transferred size */
-  transferredLength: number
-  /** compression mode */
-  zip: MadMessageCompression
   /** debug mode */
   debug: boolean
 
-  constructor(bMessage: Buffer, cb: DecoderCallback, debug: boolean) {
-    this.id = undefined
-    this.type = undefined
-    this.content = undefined
-    this.finalize = false
-    this.isResponse = true
-    this.num = 0
-
-    this.length = 0
-    this.transferredLength = 0
-    this.zip = '0'
+  constructor(debug: boolean) {
     this.debug = debug
-    this.read(bMessage, cb)
   }
 
   get_attr(attrName: keyof MadMessageDecoder) {
@@ -61,18 +45,22 @@ class MadMessageDecoder {
     const reqType = bMessage.subarray(47, 48).toString().trim() as MadMessageType
     const reqLength = parseInt(bMessage.subarray(48, 68).toString().trim(), 10)
     const reqCompression = bMessage.subarray(68, 69).toString().trim() as MadMessageCompression
-    this.id = reqId
-    this.num = parseInt(reqNum, 10)
-    this.type = reqType
-    this.length = this.transferredLength = reqLength
-    this.zip = reqCompression
-    this.finalize = parseInt(reqFinalize, 10) === 1
+    const decodedMessage: DecodedMessage = {
+      id: reqId,
+      num: parseInt(reqNum, 10),
+      type: reqType,
+      length: reqLength,
+      transferredLength: reqLength,
+      zip: reqCompression,
+      finalize: parseInt(reqFinalize, 10) === 1,
+      content: null
+    }
     switch (this.compression_supported(reqCompression)) {
       case false:
-        this.read_uncompressed({ bMessage, reqType, reqLength }, cb)
+        this.read_uncompressed({ bMessage, reqType, reqLength }, decodedMessage, cb)
         break
       default:
-        this.read_unzip(reqCompression, { bMessage, reqType, reqLength }, cb)
+        this.read_unzip(reqCompression, { bMessage, reqType, reqLength }, decodedMessage, cb)
     }
   }
 
@@ -86,17 +74,23 @@ class MadMessageDecoder {
     bMessage: Buffer,
     reqType: MadMessageType,
     reqLength: number
-  }, cb: DecoderCallback) {
+  },
+    decodedMessage: DecodedMessage,
+    cb: DecoderCallback) {
     const { bMessage, reqType, reqLength } = props
-    this.content = this.read_content(reqType, bMessage.subarray(69, reqLength + 69))
-    this.read_done(cb)
+    decodedMessage.content = this.read_content(reqType, bMessage.subarray(69, reqLength + 69))
+    this.read_done(cb, decodedMessage)
   }
 
-  read_unzip(zip: MadMessageCompression, props: {
-    bMessage: Buffer,
-    reqType: MadMessageType,
-    reqLength: number
-  }, cb: DecoderCallback) {
+  read_unzip(
+    zip: MadMessageCompression,
+    props: {
+      bMessage: Buffer,
+      reqType: MadMessageType,
+      reqLength: number
+    },
+    decodedMessage: DecodedMessage,
+    cb: DecoderCallback) {
     const { bMessage, reqType, reqLength } = props
     const zipContent = bMessage.subarray(69, reqLength + 69)
     const unzipCmd = this.unzip_cmd(zip)
@@ -108,25 +102,29 @@ class MadMessageDecoder {
 
     unzipCmd(zipContent, (e, plainContent) => {
       if (e) {
-        this.read_error(new Error(DECODER_ERRORS.ZIP_DECOMPRESSION_ERROR), cb)
+        this.read_error(new Error(DECODER_ERRORS.ZIP_DECOMPRESSION_ERROR), cb, decodedMessage)
       } else {
-        this.length = plainContent.length
-        this.content = this.read_content(reqType, plainContent)
+        decodedMessage.length = plainContent.length;
+        decodedMessage.content = this.read_content(reqType, plainContent);
         if (this.debug) {
           totalTime = Date.now() - totalTime
-          console.log(`[Decoder] ${this.zip} total decoding time: ${totalTime} ms`)
+          console.log(`[Decoder] ${decodedMessage.zip} total decoding time: ${totalTime} ms`)
         }
-        this.read_done(cb)
+        this.read_done(cb, decodedMessage)
       }
     })
   }
 
-  read_done(cb: DecoderCallback): void {
-    cb(null, this)
+  read_done(cb: DecoderCallback, decodedMessage: DecodedMessage): void {
+    cb(null, decodedMessage)
   }
 
-  read_error(e: Error, cb: DecoderCallback): void {
-    cb(e, this)
+  read_error(
+    e: Error,
+    cb: DecoderCallback,
+    decodedMessage?: DecodedMessage
+  ): void {
+    cb(e, decodedMessage)
   }
 
   unzip_cmd(zip: MadMessageCompression): ZlibMethod {
